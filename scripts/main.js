a0c6620e39bbe3862adc71525c9e23f3e216ff30
agawa98 <62704143+agawa98@users.noreply.github.com>
2022-12-19 21:07:25 +0100

    a

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ async function insertFactCat(el){
         fakcik=fakcik.replace(" ","%20")
     }
 
-    el.innerHTML += "<a id='factTranslate' target='_blank' href='https://translate.google.pl/?hl=pl&sl=en&tl=pl&text="+fakcik+"'>Przetłumacz</a>"
+    el.innerHTML += '<a id="factTranslate" target="_blank" href="https://translate.google.pl/?hl=pl&sl=en&tl=pl&text='+fakcik+'">Przetłumacz</a>'
 
 }
 
@@ -45,10 +45,60 @@ async function newCatPic(){
 }
 
 
-document.getElementById("catPic").addEventListener("click", ()=>{
-    newCatPic()
-})
 
+//CAT COMPENDIUM
+
+var breeds = [['abys','Abysiński'],['acur','American Curl'],['abob','Amerykański Bobtail'],['asho','Amerykański krótkowłosy'],['awir','Amerykański szorstkowłosy'],['tang','Angora Turecka'],['amau','Arabski Mau'],['amis','Australian Mist'],['bali','Balijski'],['bamb','Bambino'],['beng','Bengalski'],['birm','Birmański'],['bomb','Bombajski'],['bslo','Brytyjski długowłosy'],['bsho','Brytyjski krótkowłosy'],['bure','Burmski'],['buri','Burmilla'],['cspa','California Spangled'],['ctif','Chantilly-Tiffany'],['chau','Chausie'],['chee','Cheetoh'],['csho','Colorpoint krótkowłosy'],['crex','Cornish Rex'],['cymr','Cymric'],['cypr','Cypryjski'],['drex','Devon Rex'],['dons','Doński sfinks'],['lihu','Dragon Li'],['emau','Egipski Mau'],['aege','Egejski'],['esho','Egzotyczny'],['hbro','Hawański'],['hima','Himalajski'],['jbob','Japoński Bobtail'],['java','Jawajski'],['char','Kartuski'],['khao','Khao Manee'],['kora','Korat'],['kuri','Kurylski Bobtail'],['lape','LaPerm'],['mcoo','Maine Coon'],['manx','Manx'],['munc','Munchkin'],['nebe','Nebelung'],['norw','Norweski leśny'],['ocic','Ocicat'],['orie','Orientalny'],['pers','Perski'],['pixi','Pixie-bob'],['raga','Ragamuffin'],['ragd','Ragdoll'],['rblu','Rosyjski niebieski'],['sava','Savannah'],['sfol','Szkocki zwisłouchy'],['srex','Selkirk Rex'],['siam','Syjamski'],['sibe','Syberyski'],['sing','Singapurski'],['snow','Snowshoe'],['soma','Somalijski'],['sphy','Sfinks'],['tonk','Tonkijski'],['toyg','Toyger'],['tvan','Turecki van'],['ycho','York Chocolate']]
+
+var currentPhotoID = "asdasda"
+var getPhotoInt
+
+function addOptions(){
+    for(let i = 0; i<breeds.length; i++){
+        let select = document.getElementById("catCompBreedSelect")
+        let option = document.createElement("option")
+        option.className = "breed"
+        option.value = breeds[i][0]
+        option.innerText = breeds[i][1]
+        
+        select.appendChild(option)
+    }
+}
+
+async function showInfo(){
+    clearInterval(getPhotoInt)
+    let catID = document.getElementById("catCompBreedSelect").value
+    console.log(catID)
+    
+    getBreedPhoto(catID)
+    getPhotoInt = setInterval(async ()=>{
+        getBreedPhoto(catID)
+    }, 3000)
+
+    getBreedInfo(catID)
+}
+
+async function getBreedPhoto(catID){
+    var res = await fetch("https://api.thecatapi.com/v1/images/search?breed_ids="+catID)
+    var data = await res.json()
+    if(data[0].id == currentPhotoID){
+       //  console.log(document.getElementById("catCompWrapper").clientHeight + " < "+data[0].height)                                   TODO
+        getBreedPhoto(catID)
+    }
+    document.getElementById("catCompImg").src = data[0].url
+    currentPhotoID = data[0].id
+}
+
+async function getBreedInfo(catID){
+    var res = await fetch("https://api.thecatapi.com/v1/breeds/"+catID)
+    var data = await res.json()
+    
+    document.getElementById("catCompOrigin").innerText = data.origin
+    document.getElementById("catCompDesc").innerText = data.description
+}
+
+
+document.getElementById("catCompBreedSelect").addEventListener("change",showInfo)
 
 
 //CAT GENERATOR
@@ -65,14 +115,16 @@ async function generateCat(){
 async function getCatTags(){
     var res = await fetch("https://cataas.com/api/tags")
     var data = await res.json()
-    console.log(data)
 
-    for(let i=1; i<data.length; i++){
+    for(let i=3; i<data.length-1; i++){
         let input = document.getElementById("catGenTagSelect")
-        let option = document.createElement("option")
-        option.value = data[i]
-        option.textContent = data[i]
-        input.appendChild(option)
+        if(data[i].includes(" ")==false){
+            let option = document.createElement("option")
+            option.value = data[i]
+            option.textContent = data[i]
+            input.appendChild(option)
+        }
+        
     }
 }
 
@@ -89,26 +141,30 @@ async function returnTag(){
         
 }
 
-function catGenHelp(){
-    let box = document.getElementById("catGenHelpBox")
-    let icon = document.getElementById("catGenTagSelectQuestion")
-    box.style.display = "block"
-    box.style.left = window.scrollX+icon.getBoundingClientRect().right - box.clientWidth - icon.clientWidth*2 +"px"
-    box.style.top = window.scrollY + icon.getBoundingClientRect().top +"px"
-
-}
 
 
-document.getElementById("catGenButton").addEventListener("click", async()=>{
-    generateCat()
-})
+// przycisk pokazujacy ze nie ma danego tagu dla kota
 
-document.getElementById("catGenTagSelectQuestion").addEventListener("mouseover", catGenHelp)
+// function catGenHelp(){
+//     let box = document.getElementById("catGenHelpBox")
+//     let icon = document.getElementById("catGenTagSelectQuestion")
+//     box.style.display = "block";
+    
+//     box.style.left = window.scrollX+icon.getBoundingClientRect().right - box.clientWidth - icon.clientWidth*2 +"px"
+//     box.style.top = window.scrollY + icon.getBoundingClientRect().top +"px"
 
-document.getElementById("catGenTagSelectQuestion").addEventListener("mouseout",()=>{
-    document.getElementById("catGenHelpBox").style.display = "none"
-    console.log("asdasd")
-} )
+// }
+
+
+// document.getElementById("catGenButton").addEventListener("click", async()=>{
+//     generateCat()
+// })
+
+// document.getElementById("catGenTagSelectQuestion").addEventListener("mouseover", catGenHelp)
+
+// document.getElementById("catGenTagSelectQuestion").addEventListener("mouseout",()=>{
+//     document.getElementById("catGenHelpBox").style.display = "none";
+// } )
 
 
 
@@ -155,6 +211,8 @@ window.addEventListener("load",()=>{
 
     getCatTags()
     
+    // dodawanie opcji w wybieraniu rasy
+    addOptions()
 
     //wpisanie fakciku o kotkach
     insertFactCat(document.getElementById("catFactText"))
